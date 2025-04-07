c20e1c0b5af39d7dd68aa7e3e843b6384cfe47d5
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-04-07 14:34:26 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,32 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+  let numberSelected = numbers[Math.floor(Math.random()*numbers.length)]
+
+  document.querySelector(".number").innerHTML = numberSelected;
+
+  let suits = ["♦", "♥", "♠", "♣"]
+
+  let suitsIndex = Math.floor(Math.random()*suits.length);
+
+  let suitsSelected =suits[suitsIndex];
+  
+
+  let suitsOnHTML = document.querySelectorAll(".simbols");
+  suitsOnHTML.forEach(x=>{
+
+    x.innerHTML=suitsSelected;
+
+    if(suitsIndex>1){
+      x.style.color = "black";
+    }
+    else{
+      x.style.color = "red";
+    }
+
+  })
+
 };
